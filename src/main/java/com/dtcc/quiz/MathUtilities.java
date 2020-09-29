3ba7960f9f4d3875ed17cbb968648a57d4d6500f
RWLangwig <rlangwig@verizon.net>
2020-09-28 21:29:47 -0400

    MathUtilities update

--- src/main/java/com/dtcc/quiz/MathUtilities.java
+++ src/main/java/com/dtcc/quiz/MathUtilities.java
@@ -7,8 +7,10 @@ public class MathUtilities {
      * @param valueToAdd number
      * @return the sum of the two numbers
      */
+	
     public Integer add(Integer baseValue, Integer valueToAdd){
-        return null;
+        int sum = (baseValue + valueToAdd);
+    	return sum;
     }
 
     /**
@@ -17,17 +19,22 @@ public class MathUtilities {
      * @param valueToAdd second number
      * @return the sum of the two numbers
      */
+    
+    
     public Double add(Double baseValue, Double valueToAdd){
-        return null;
-    }
+        double sum = baseValue + valueToAdd ;
+        return sum;
+        }
 
     /**
      * Get half the value of the number
      * @param number the number given
      * @return the half of the number in double
      */
+    
     public Double half(Integer number) {
-        return null;
+    	double half = number / 2 ;
+        return half;
     }
 
     /**
@@ -35,11 +42,13 @@ public class MathUtilities {
      * @param number the number given
      * @return true if the number is odd, false if it is even
      */
+    
     public Boolean isOdd(Integer number){
-       return false;
-
+    	if (number % 2 == 1) {
+    		return true;
     }
-
+    	else return false;
+          }
 
     /**
      * Multiply the number by itself
@@ -47,6 +56,7 @@ public class MathUtilities {
      * @return the result of the number multiply by itself
      */
     public Integer square(Integer number) {
-        return null;
+    	   	
+        return number*number;
     }
 }
